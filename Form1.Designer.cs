ad41cc04f5ab6434c8ffb598248c569d339d5865
Bahattin <72936563+BahattinCinar@users.noreply.github.com>
2023-06-08 02:45:41 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace DIziler_ile_hesap_makinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(70, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(66, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Toplama";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(161, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(66, 17);
+            this.radioButton2.TabIndex = 0;
+            this.radioButton2.Text = "Cikartma";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(252, 12);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(61, 17);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.Text = "Carpma";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(343, 12);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(54, 17);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.Text = "Bolme";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 68);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Baslat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 229);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace DIziler_ile_hesap_makinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i , giris_sayisi, toplam = 0, carpim = 1 , bolum = 1, cikartma = 0;
+
+            try
+            {
+                if (radioButton1.Checked == false && radioButton2.Checked == false && radioButton3.Checked == false && radioButton4.Checked == false)
+                {
+                    MessageBox.Show("bir islem seciniz");
+                }
+
+                giris_sayisi = Convert.ToInt32(Interaction.InputBox("Giris yapicaginiz sayi miktari", "Kac Sayi Girilecek", "", 125, 125));
+
+                int[] num = new int[giris_sayisi];
+
+                if (radioButton1.Checked == true)
+                {
+                    for (i = 0 ; i <= num.Length - 1; i++)
+                    {
+                        num[i] = Convert.ToInt32(Interaction.InputBox($"{i + 1}. Sayi girisi", "Sayi Giris Ekrani", "", 250, 250));
+                        toplam += num[i];
+                    }
+
+                    label1.Text = $"Girdiginiz sayilarin toplami = {toplam}";
+                }
+
+                else if (radioButton3.Checked == true)
+                {
+                    for (i = 0; i <= num.Length-1; i++)
+                    {
+                        num[i] = Convert.ToInt32(Interaction.InputBox($"{i + 1}. Sayi girisi", "Sayi Giris Ekrani", "", 250, 250));
+                        carpim *= num[i];
+                    }
+
+                    label1.Text = $"Girdiginiz sayilarin carpimi = {carpim}";
+                }
+
+                else if (radioButton4.Checked == true)
+                {
+                    for (i = 0; i <= num.Length-1; i++)
+                    {
+                        num[i] = Convert.ToInt32(Interaction.InputBox($"{i + 1}. Sayi girisi", "Sayi Giris Ekrani", "", 250, 250));
+                        bolum /= num[i];
+                    }
+
+                    label1.Text = $"Girdiginiz sayilarin bolumu = {bolum}";
+                }
+
+                else
+                {
+                    for (i = 0; i <= num.Length - 1; i++)
+                    {
+                        num[i] = Convert.ToInt32(Interaction.InputBox($"{i + 1}. Sayi girisi", "Sayi Giris Ekrani", "", 250, 250));
+                        cikartma = -num[i] - cikartma;
+                    }
+
+                    label1.Text = $"Girdiginiz sayilarin bolumu = {cikartma}";
+                }
+            }
+
+            catch (FormatException error1)
+            {
+                MessageBox.Show("Tam sayi giriniz");
+                MessageBox.Show(error1.Message);
+            }
+
+            catch(OverflowException error2)
+            {
+                MessageBox.Show("sayi araliginiz -2,147,483,648 ve 2,147,483,647 arasinda olmali");
+                MessageBox.Show(error2.Message);
+            }
+
+            catch
+            {
+                MessageBox.Show("Beklenmedik bir hata ile karsilastiniz");
+            }
+
+            finally
+            {
+                radioButton1.Checked = false;
+                radioButton2.Checked = false;
+                radioButton3.Checked = false;
+                radioButton4.Checked = false;    
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
